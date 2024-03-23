dca67fdb4934d814d00e1955e7f6162a685eba12
Mikel-37 <mishakill37@gmail.com>
2024-03-23 13:42:59 +0300

    install react js and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App(props) {
+    return (
+        <h1 className='text-3xl text-red-500'>SSSS</h1>
+    );
+}
+
+export default App;                                                                                                      
